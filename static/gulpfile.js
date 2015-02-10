af8c96a0a83a6fc8661cfd7f7510666335a238f5
jacquestardie <hi@jacquestardie.org>
2015-02-09 22:54:45 -0800

    add datcss for styles and update templates

---
+++ static/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp                = require('gulp')
+var postcss             = require('gulp-postcss')
+var autoprefixer        = require('autoprefixer-core')
+var atImport            = require('postcss-import')
+var customProperties    = require('postcss-custom-properties')
+var fontVariants        = require('postcss-font-variant')
+var customMedia         = require('postcss-custom-media')
+var rename              = require('gulp-rename')
+var minify              = require('gulp-minify-css')
+
+
+gulp.task('default', function () {
+    var processors = [
+        atImport(),
+        fontVariants(),
+        autoprefixer({ browsers: ['last 3 versions']}),
+        customProperties(),
+        customMedia()
+    ];
+    return gulp.src('./src/index.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./'))
+        .pipe(minify())
+        .pipe(rename('index.min.css'))
+        .pipe(gulp.dest('./'))
+})
