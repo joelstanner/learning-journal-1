935b145200e880f4809fb5a42e4b4c97986515a8
jacquestardie <hi@jacquestardie.org>
2015-02-11 20:38:38 -0800

    add edit functionality.

--- journal.py
+++ journal.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 import os
 import logging
 import psycopg2
@@ -6,6 +7,8 @@ import markdown
 import pygments
 import jinja2
 import datetime
+from datetime import date
+
 from pyramid.config import Configurator
 from pyramid.session import SignedCookieSessionFactory
 from pyramid.view import view_config
@@ -19,7 +22,6 @@ from waitress import serve
 from contextlib import closing
 
 here = os.path.dirname(os.path.abspath(__file__))
-
 logging.basicConfig()
 log = logging.getLogger(__file__)
 
@@ -38,27 +40,20 @@ DB_ENTRIES_LIST = '''
 INDIVIDUAL_ENTRY = '''
     SELECT id, title, text, created FROM entries WHERE id = %s '''
 
-def md(input):
-    return markdown.markdown(input, extension=['CodeHilite'])
-
 def connect_db(settings):
-    '''Return a connection to the configured database'''
     return psycopg2.connect(settings['db'])
 
 def init_db():
-    '''Create database tables defined by DB_SCHEMA'''
     settings = {}
-    settings['db'] = os.environ.get(
-            'DATABASE_URL', 'dbname=learning-journal user=Jacques'
-            )
+    settings['db'] = os.environ.get('DATABASE_URL', 'dbname=learning-journal user=Jacques')
     settings['auth.username'] = os.environ.get('AUTH_USERNAME', 'admin')
     settings['auth.password'] = os.environ.get('AUTH_PASSWORD', 'secret')
+
     with closing(connect_db(settings)) as db:
         db.cursor().execute(DB_SCHEMA)
         db.commit()
 
 def close_connection(request):
-    '''Close the database connection for this request.'''
     db = getattr(request, 'db', None)
     if db is not None:
         if request.exception is not None:
@@ -88,43 +83,8 @@ def open_connection(event):
     request.db = connect_db(settings)
     request.add_finished_callback(close_connection)
 
-def write_entry(request):
-    title = request.params.get('title', None)
-    text = request.params.get('text', None)
-    created = datetime.datetime.utcnow()
-    request.db.cursor().execute(INSERT_ENTRY, [title, text, created])
-
-
-@view_config(route_name='home', renderer='templates/list.jinja2')
-def read_entries(request):
-    cur = request.db.cursor()
-    cur.execute(DB_ENTRIES_LIST)
-    keys = ('id', 'title', 'text', 'created')
-    entries = [dict(zip(keys, row)) for row in cur.fetchall()]
-    for entry in entries:
-        entry['text'] = markdown.markdown(entry['text'], extensions=['codehilite', 'fenced_code'])
-    return {'entries': entries }
-
-
-@view_config(route_name='entry', renderer='templates/list.jinja2')
-def read_entry(request):
-    id = request.matchdict.get('id', None)
-    cur = request.db.cursor()
-    cur.execute(INDIVIDUAL_ENTRY, [id])
-    keys = ('id', 'title', 'text', 'created')
-    entries = [dict(zip(keys, row)) for row in cur.fetchall()]
-    for entry in entries:
-        entry['text'] = markdown.markdown(entry['text'], extensions=['codehilite', 'fenced_code'])
-    return {'entries': entries }
-
-@view_config(route_name='add', request_method='POST')
-def add_entry(request):
-    try:
-        write_entry(request)
-    except psycopg2.Error:
-        return HTTPInternalServerError
-
-    return HTTPFound(request.route_url('home'))
+def md(input):
+    return markdown.markdown(input)
 
 @view_config(route_name='login', renderer='templates/login.jinja2')
 def login(request):
@@ -150,9 +110,62 @@ def logout(request):
     headers = forget(request)
     return HTTPFound(request.route_url('home'), headers=headers)
 
-def main():
-    '''Create a configured wsgi app'''
+@view_config(route_name='home', renderer='templates/list.jinja2')
+def read_entries(request):
+    cur = request.db.cursor()
+    cur.execute(DB_ENTRIES_LIST)
+    keys = ('id', 'title', 'text', 'created')
+    entries = [dict(zip(keys, row)) for row in cur.fetchall()]
+    for entry in entries:
+        entry['text'] = markdown.markdown(entry['text'], extensions=['codehilite', 'fenced_code'])
+    return {'entries': entries }
 
+@view_config(route_name='add', request_method='POST')
+def add(request):
+    try:
+        title = request.params.get('title', None)
+        text = request.params.get('text', None)
+        created = datetime.datetime.utcnow()
+        request.db.cursor().execute(INSERT_ENTRY, [title, text, created])
+    except psycopg2.Error:
+        return HTTPInternalServerError
+    return HTTPFound(request.route_url('home'))
+
+@view_config(route_name='entry', renderer='templates/entry.jinja2')
+def read(request):
+    id = request.matchdict.get('id', None)
+    cur = request.db.cursor()
+    cur.execute(INDIVIDUAL_ENTRY, [id])
+    keys = ('id', 'title', 'text', 'created')
+    entries = [dict(zip(keys, row)) for row in cur.fetchall()]
+    for entry in entries:
+        entry['text'] = markdown.markdown(entry['text'], extensions=['codehilite', 'fenced_code'])
+    return {'entries': entries }
+
+@view_config(route_name='edit', renderer='templates/edit.jinja2')
+def edit(request):
+    """return a list of all entries as dicts"""
+    id = request.matchdict.get('id', -1)
+    cursor = request.db.cursor()
+    cursor.execute(INDIVIDUAL_ENTRY, (id,))
+    keys = ('id', 'title', 'text', 'created')
+    entries = [dict(zip(keys, row)) for row in cursor.fetchall()]
+    return {'entries': entries}
+
+@view_config(route_name='update', request_method='POST')
+def update(request):
+    try:
+        id = request.matchdict.get('id', -1)
+        title = request.params.get('title', None)
+        text = request.params.get('text', None)
+        created = date.today()
+        ENTRY_UPDATE = "UPDATE entries SET title=%s, text=%s, created=%s WHERE id=%s;"
+        request.db.cursor().execute(ENTRY_UPDATE, [title, text, created, id])
+    except psycopg2.Error:
+        return HTTPInternalServerError
+    return HTTPFound(request.route_url('home'))
+
+def main():
     manager = BCRYPTPasswordManager()
 
     settings = {}
@@ -167,30 +180,32 @@ def main():
     session_factory = SignedCookieSessionFactory(secret)
     auth_secret = os.environ.get('JOURNAL_AUTH_SECRET', 'anotherseekrit')
 
+    jinja2.filters.FILTERS['markdown'] = md
+
     # configuration setup
     config = Configurator(
-            settings=settings,
-            session_factory=session_factory,
-            authentication_policy=AuthTktAuthenticationPolicy(
-                secret=auth_secret,
-                hashalg='sha512'
-                ),
-            authorization_policy=ACLAuthorizationPolicy(),
-            )
-    jinja2.filters.FILTERS['markdown'] = md
+        settings=settings,
+        session_factory=session_factory,
+        authentication_policy=AuthTktAuthenticationPolicy(
+            secret=auth_secret,
+            hashalg='sha512'
+            ),
+        authorization_policy=ACLAuthorizationPolicy(),
+        )
     config.include('pyramid_jinja2')
     config.add_static_view('static', os.path.join(here, 'static'))
+
     config.add_route('home', '/')
-    config.add_route('add', '/add')
-    config.add_route('entry', '/post/{id}')
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
+    config.add_route('add', '/add')
+    config.add_route('entry', '/post/{id}')
+    config.add_route('edit', '/edit/{id}')
+    config.add_route('update','/update/{id}')
     config.scan()
 
     app = config.make_wsgi_app()
-
     return app
-
 
 if __name__ == '__main__':
     app = main()
